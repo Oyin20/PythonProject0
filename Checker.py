021ce47cb68a7d0633c0ae0ff2743e1dfdd65cb2
Oyin20 <128806301+Oyin20@users.noreply.github.com>
2023-03-31 07:42:04 -0600

    Update Checker.py

--- Checker.py
+++ Checker.py
@@ -1,23 +1,3 @@
-password = input("Enter password: ")
-
-has_uppercase = False
-has_number = False
-
-if len(password) >= 8:
-    for c in password:
-        if c.isupper():
-            has_uppercase = True
-        elif c.isdigit():
-            has_number = True
-
-    if has_uppercase and has_number:
-        print("Strong password!")
-    elif has_uppercase or has_number:
-        print("Moderate password!")
-    else:
-        print("Weak password!")
-else:
-    print("Password must be at least 8 characters long!")
 import re
 
 def check_password_strength(password):
@@ -28,3 +8,8 @@ def check_password_strength(password):
 
     Args:
         password (str): The password to be checked.
+    Returns:
+        int: 0 if password is weak, 1 if password is moderate and 2 if password is strong. """
+ strength = 0
+ if len(password) < 8:
+        print("Password is w
